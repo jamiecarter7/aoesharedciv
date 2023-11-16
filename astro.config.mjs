7069c22fe577873d816fa9054a8e31ae99a2b397
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-11-16 23:10:07 +0000

    add a site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
-  base: '/aoesharedcivbonus',
+  base: "/aoesharedcivbonus",
+  site: "https://maybelatergames.co.uk/aoesharedcivbonus",
+  integrations: [tailwind(), solidJs(), sitemap()],
 });
